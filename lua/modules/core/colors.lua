b4e8f2ed589e5f283d58722d2182fe55d98ffa09
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-04-15 16:32:31 +0000

    Format with stylua

--- lua/modules/core/colors.lua
+++ lua/modules/core/colors.lua
@@ -1,22 +1,22 @@
 vim.g.material_style = 'darker'
 
 require('material').setup({
-    italics = {
-        comments = true,
-        functions = true,
-    },
-    contrast_filetypes = {
-        'terminal',
-        'packer',
-        'qf',
-    },
-    high_visibility = {
-        lighter = false,
-        darker = false,
-    },
-    disable = {
-        background = true,
-    },
+  italics = {
+    comments = true,
+    functions = true,
+  },
+  contrast_filetypes = {
+    'terminal',
+    'packer',
+    'qf',
+  },
+  high_visibility = {
+    lighter = false,
+    darker = false,
+  },
+  disable = {
+    background = true,
+  },
 })
 
 vim.cmd([[colorscheme material]])
