2070360e4d5e2962e032167193afcea5643da55d
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-04-16 22:42:20 +0000

    Format with stylua

--- lua/modules/plugins/telescope.lua
+++ lua/modules/plugins/telescope.lua
@@ -1,34 +1,34 @@
 require('telescope').setup({
-    defaults = {
-        prompt_prefix = '❯ ',
-        selection_caret = '❯ ',
-        sorting_strategy = 'ascending',
-        extensions = {
-            fzf = {
-                fuzzy = true,
-                override_generic_sorter = true,
-                override_file_sorter = true,
-            },
-        },
-        vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-            '--ignore',
-            '--hidden',
-            '-g',
-            '!.git',
-        },
-        mappings = {
-            i = {
-                ['<ESC>'] = require('telescope.actions').close,
-                ['<C-u>'] = false,
-                ['<C-d>'] = false,
-            },
-        },
+  defaults = {
+    prompt_prefix = '❯ ',
+    selection_caret = '❯ ',
+    sorting_strategy = 'ascending',
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+      },
     },
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--ignore',
+      '--hidden',
+      '-g',
+      '!.git',
+    },
+    mappings = {
+      i = {
+        ['<ESC>'] = require('telescope.actions').close,
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
 })
