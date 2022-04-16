7e01ed37a59b96446ff64fc7310b3ed3b0304ff1
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-04-16 22:13:02 +0000

    Format with stylua

--- lua/modules/core/colors.lua
+++ lua/modules/core/colors.lua
@@ -2,28 +2,28 @@
 require('nightfox').setup({
   options = {
     terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = true,   -- Non focused panes set to alternative background
-    styles = {              -- Style to be applied to different syntax groups
-      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
-      conditionals = "italic",
-      constants = "bold",
-      functions = "italic",
-      keywords = "NONE",
-      numbers = "bold",
-      operators = "NONE",
-      strings = "italic",
-      types = "italic",
-      variables = "NONE",
+    dim_inactive = true, -- Non focused panes set to alternative background
+    styles = { -- Style to be applied to different syntax groups
+      comments = 'italic', -- Value is any valid attr-list value `:help attr-list`
+      conditionals = 'italic',
+      constants = 'bold',
+      functions = 'italic',
+      keywords = 'NONE',
+      numbers = 'bold',
+      operators = 'NONE',
+      strings = 'italic',
+      types = 'italic',
+      variables = 'NONE',
     },
-    inverse = {             -- Inverse highlight for different types
+    inverse = { -- Inverse highlight for different types
       match_paren = false,
       visual = false,
       search = false,
     },
-    modules = {             -- List of various plugins and additional options
+    modules = { -- List of various plugins and additional options
       -- ...
     },
-  }
+  },
 })
 
 vim.cmd('colorscheme nightfox')
