a9b2f0e418b26fbb13187f1e4703c9b8dd691492
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-04-16 19:31:01 +0000

    Format with stylua

--- lua/modules/plugins/treesitter.lua
+++ lua/modules/plugins/treesitter.lua
@@ -1,7 +1,7 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+require('nvim-treesitter.configs').setup({
+  ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
-    enable = true,              -- false will disable the whole extension
+    enable = true, -- false will disable the whole extension
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -18,12 +18,12 @@ require'nvim-treesitter.configs'.setup {
       toggle_hl_groups = 'i',
       toggle_injected_languages = 't',
       toggle_anonymous_nodes = 'a',
-     toggle_language_display = 'I',
+      toggle_language_display = 'I',
       focus_language = 'f',
       unfocus_language = 'F',
       update = 'R',
       goto_node = '<cr>',
       show_help = '?',
     },
-  }
-}
+  },
+})
