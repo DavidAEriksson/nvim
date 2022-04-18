0d27f2adb78ac85c3ecc785188930eaaa214cc24
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-04-18 12:35:26 +0000

    Format with stylua

--- lua/modules/plugins/pack.lua
+++ lua/modules/plugins/pack.lua
@@ -20,168 +20,169 @@ require('packer').init({
   compile_path = install_path .. '/packer_compiled.lua',
 })
 
-return require('packer').startup({function(use)
-  -- Plugin manager
-  use('wbthomason/packer.nvim')
+return require('packer').startup({
+  function(use)
+    -- Plugin manager
+    use('wbthomason/packer.nvim')
 
-  -- Sanity libraries
-  use({
-    'nvim-lua/plenary.nvim',
-    'nvim-lua/popup.nvim',
-    'ThePrimeagen/harpoon',
-  })
-
-  --- Lsp
-  use({
-    'neovim/nvim-lspconfig',
-    'ray-x/lsp_signature.nvim',
-    'jose-elias-alvarez/nvim-lsp-ts-utils',
-    'onsails/lspkind-nvim',
-    'williamboman/nvim-lsp-installer',
-    'omnisharp/omnisharp-vim',
-  })
-
-  use({
-    'jose-elias-alvarez/null-ls.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim',
-      'neovim/nvim-lspconfig',
-    },
-  })
-
-  -- Completion
-  use({
-    'hrsh7th/nvim-cmp',
-    'saadparwaiz1/cmp_luasnip',
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-nvim-lua',
-    'hrsh7th/cmp-nvim-lsp-signature-help',
-  })
-
-  -- Snippets
-  use({
-    'L3MON4D3/luasnip',
-    requires = {
-      'rafamadriz/friendly-snippets',
-    },
-  })
-
-  --- Format/Lint
-  use({ 'mhartington/formatter.nvim' })
-
-  --- Telescope
-  use({
-    'nvim-telescope/telescope.nvim',
-    requires = {
+    -- Sanity libraries
+    use({
       'nvim-lua/plenary.nvim',
       'nvim-lua/popup.nvim',
-      'kdheepak/lazygit.nvim',
-    },
-  })
+      'ThePrimeagen/harpoon',
+    })
 
-  use({
-    'nvim-telescope/telescope-project.nvim',
-    requires = {
+    --- Lsp
+    use({
+      'neovim/nvim-lspconfig',
+      'ray-x/lsp_signature.nvim',
+      'jose-elias-alvarez/nvim-lsp-ts-utils',
+      'onsails/lspkind-nvim',
+      'williamboman/nvim-lsp-installer',
+      'omnisharp/omnisharp-vim',
+    })
+
+    use({
+      'jose-elias-alvarez/null-ls.nvim',
+      requires = {
+        'nvim-lua/plenary.nvim',
+        'neovim/nvim-lspconfig',
+      },
+    })
+
+    -- Completion
+    use({
+      'hrsh7th/nvim-cmp',
+      'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
+    })
+
+    -- Snippets
+    use({
+      'L3MON4D3/luasnip',
+      requires = {
+        'rafamadriz/friendly-snippets',
+      },
+    })
+
+    --- Format/Lint
+    use({ 'mhartington/formatter.nvim' })
+
+    --- Telescope
+    use({
       'nvim-telescope/telescope.nvim',
-    },
-  })
+      requires = {
+        'nvim-lua/plenary.nvim',
+        'nvim-lua/popup.nvim',
+        'kdheepak/lazygit.nvim',
+      },
+    })
 
-  --- Treesitter
-  use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  })
+    use({
+      'nvim-telescope/telescope-project.nvim',
+      requires = {
+        'nvim-telescope/telescope.nvim',
+      },
+    })
 
-  use({
-    'windwp/nvim-ts-autotag',
-    ft = { 'typescript', 'typescriptreact' },
-  })
+    --- Treesitter
+    use({
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate',
+    })
 
-  use({
-    'JoosepAlviste/nvim-ts-context-commentstring',
-    ft = { 'typescript', 'typescriptreact' },
-  })
+    use({
+      'windwp/nvim-ts-autotag',
+      ft = { 'typescript', 'typescriptreact' },
+    })
 
-  use({
-    'numToStr/Comment.nvim'
-  })
+    use({
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      ft = { 'typescript', 'typescriptreact' },
+    })
 
-  use({
-    'tpope/vim-surround',
-    'windwp/nvim-autopairs',
-    'norcalli/nvim-colorizer.lua',
-  })
+    use({
+      'numToStr/Comment.nvim',
+    })
 
-  use({
-    'kkoomen/vim-doge',
-    run = ':call doge#install()',
-  })
+    use({
+      'tpope/vim-surround',
+      'windwp/nvim-autopairs',
+      'norcalli/nvim-colorizer.lua',
+    })
 
-  -- Colors, indentation and blankline
-  use({
-    'EdenEast/nightfox.nvim',
-    'kyazdani42/nvim-web-devicons',
-    'lukas-reineke/indent-blankline.nvim',
-  })
+    use({
+      'kkoomen/vim-doge',
+      run = ':call doge#install()',
+    })
 
-  -- Statusline
-  use({
-    'nvim-lualine/lualine.nvim',
-    requires = {
-      'nvim-web-devicons',
-      opt = true,
-    },
-  })
-
-  use({ 'arkav/lualine-lsp-progress' })
-
-  -- Explorer
-  use({
-    'kyazdani42/nvim-tree.lua',
-    requires = {
+    -- Colors, indentation and blankline
+    use({
+      'EdenEast/nightfox.nvim',
       'kyazdani42/nvim-web-devicons',
-    },
-  })
+      'lukas-reineke/indent-blankline.nvim',
+    })
 
-  -- Alpha welcome screen
-  use({
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = function()
-      require('alpha').setup(require('alpha.themes.startify').config)
-    end,
-  })
+    -- Statusline
+    use({
+      'nvim-lualine/lualine.nvim',
+      requires = {
+        'nvim-web-devicons',
+        opt = true,
+      },
+    })
 
-  -- Floating terminal
-  use({
-    'voldikss/vim-floaterm',
-  })
+    use({ 'arkav/lualine-lsp-progress' })
 
-  -- Bufferline
-  use({
-    'akinsho/bufferline.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-  })
+    -- Explorer
+    use({
+      'kyazdani42/nvim-tree.lua',
+      requires = {
+        'kyazdani42/nvim-web-devicons',
+      },
+    })
 
-  -- Git
-  use({
-    'airblade/vim-rooter',
-    'tpope/vim-fugitive',
-    'APZelos/blamer.nvim',
-    'lewis6991/gitsigns.nvim',
-  })
+    -- Alpha welcome screen
+    use({
+      'goolord/alpha-nvim',
+      requires = { 'kyazdani42/nvim-web-devicons' },
+      config = function()
+        require('alpha').setup(require('alpha.themes.startify').config)
+      end,
+    })
 
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end,
-config = {
-  display = {
-    open_fn = function ()
-      return require('packer.util').float({ border = 'single' })
+    -- Floating terminal
+    use({
+      'voldikss/vim-floaterm',
+    })
+
+    -- Bufferline
+    use({
+      'akinsho/bufferline.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons' },
+    })
+
+    -- Git
+    use({
+      'airblade/vim-rooter',
+      'tpope/vim-fugitive',
+      'APZelos/blamer.nvim',
+      'lewis6991/gitsigns.nvim',
+    })
+
+    if packer_bootstrap then
+      require('packer').sync()
     end
-  }
-}
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require('packer.util').float({ border = 'single' })
+      end,
+    },
+  },
 })
