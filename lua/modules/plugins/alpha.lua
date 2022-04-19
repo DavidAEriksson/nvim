eafcffe600e4b84ca72a89cb482dd937feb18e92
David <david.eriksson@valtech.com>
2022-04-19 13:21:45 +0200

    feat(alpha): add packer sync with plugin count

--- lua/modules/plugins/alpha.lua
+++ lua/modules/plugins/alpha.lua
@@ -1,5 +1,6 @@
 local alpha = require('alpha')
 local dashboard = require('alpha.themes.dashboard')
+local plugins_count = vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1))
 
 local function footer()
   local datetime = os.date(' %d-%m-%Y   %H:%M:%S')
@@ -30,10 +31,11 @@ dashboard.section.header.val = {
 -- Set menu
 dashboard.section.buttons.val = {
   dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
-  dashboard.button('f', '  > Find file', ':cd $HOME/Development | Telescope find_files<CR>'),
+  dashboard.button('f', '  > Find file', ':Telescope find_files<CR>'),
   dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
   dashboard.button('s', '  > Settings', ':e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>'),
   dashboard.button('q', '  > Quit NVIM (cringe)', ':qa<CR>'),
+  dashboard.button('p', '  > Sync all (' .. plugins_count .. ') plugins', ':PackerSync<CR>'),
 }
 
 dashboard.section.footer.val = footer()
