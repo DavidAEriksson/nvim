3c5ee27fd3799c64c63e46eb134a7a7fc695d415
David <david.eriksson@valtech.com>
2022-05-08 17:39:38 +0200

    feat(telescope): update telescope layout

--- lua/modules/plugins/telescope.lua
+++ lua/modules/plugins/telescope.lua
@@ -1,7 +1,8 @@
 require('telescope').setup({
   defaults = {
-    prompt_prefix = '❯ ',
+    prompt_prefix = '  ',
     selection_caret = '❯ ',
+    entry_prefix = '  ',
     sorting_strategy = 'ascending',
     extensions = {
       fzf = {
@@ -30,5 +31,41 @@ require('telescope').setup({
         ['<C-d>'] = false,
       },
     },
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    layout_strategy = "horizontal",
+    layout_config = {
+         horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+         },
+         vertical = {
+            mirror = false,
+         },
+         width = 0.87,
+         height = 0.80,
+         preview_cutoff = 120,
+    },
+    file_sorter = require('telescope.sorters').get_fuzzy_file,
+    generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
+    path_display = { "truncate" },
+    winblend = 0,
+    border = {},
+    borderchars = { "", "", "", "", "", "", "", "" },
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
   },
 })
+
+local extensions = { "themes" , "terms" }
+
+pcall(function()
+  for _, ext in pairs(extensions) do
+    require('telescope').load_extension(ext)
+  end
+end)
