ee8b3dd7803ae0833d1aa56418898da1aaba31ed
David <david.eriksson@valtech.com>
2022-05-09 20:26:18 +0200

    chore: move M to top of file in sumneko_lua

--- lua/modules/lsp/sumneko_lua.lua
+++ lua/modules/lsp/sumneko_lua.lua
@@ -1,8 +1,10 @@
+local M = {}
+
+
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-local M = {}
 
 M.setup = function(on_attach, capabilities)
   local lspconfig = require('lspconfig')
