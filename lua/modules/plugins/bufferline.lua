a5539b2947bf132bd1e1d9ea9434751536b52543
David <david.eriksson@valtech.com>
2022-05-12 10:49:03 +0200

    feat(bufferline): set style to block(thin) and fix numbering

--- lua/modules/plugins/bufferline.lua
+++ lua/modules/plugins/bufferline.lua
@@ -1,12 +1,10 @@
 require('bufferline').setup({
   options = {
-    numbers = function(opts)
-      return string.format('%s|%s.)', opts.id, opts.raise(opts.ordinal))
-    end,
+    numbers = 'ordinal',
     tab_size = 4,
     diagnostics = 'nvim_lsp',
     diagnostics_update_in_insert = true,
-    separator_style = 'slant',
+    separator_style = 'thin',
     show_close_icon = false,
   },
 })
