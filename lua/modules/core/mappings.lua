b73b3f9ac9c35f6286db237c163dd78db903e716
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-05-18 08:07:24 +0000

    Format with stylua

--- lua/modules/core/mappings.lua
+++ lua/modules/core/mappings.lua
@@ -115,7 +115,27 @@ nmap('<leader>tt', '<cmd>:TroubleToggle<CR>')
 nmap('<leader>tw', '<cmd>:TroubleToggle workspace_diagnostics<CR>')
 nmap('<leader>td', '<cmd>:TroubleToggle document_diagnostics<CR>')
 
-vim.api.nvim_set_keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<leader>rf", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap(
+  'v',
+  '<leader>re',
+  [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
+  { noremap = true, silent = true, expr = false }
+)
+vim.api.nvim_set_keymap(
+  'v',
+  '<leader>rf',
+  [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
+  { noremap = true, silent = true, expr = false }
+)
+vim.api.nvim_set_keymap(
+  'v',
+  '<leader>rv',
+  [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
+  { noremap = true, silent = true, expr = false }
+)
+vim.api.nvim_set_keymap(
+  'v',
+  '<leader>ri',
+  [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
+  { noremap = true, silent = true, expr = false }
+)
