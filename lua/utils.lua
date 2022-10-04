07d9e970034e35d0166a36ef045279de0e9d18d3
David Eriksson <david.eriksson@valtech.com>
2022-10-04 14:50:12 +0200

    chore: cleanup unused utils

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,7 +1,3 @@
--- Incredible utility functions from https://github.com/n3wborn/nvim
-
-local format = string.format
-local uv = vim.loop
 local api = vim.api
 
 local get_map_options = function(custom_options)
@@ -14,51 +10,16 @@ end
 
 local M = {}
 
-M.map = function(mode, target, source, opts)
-  vim.keymap.set(mode, target, source, get_map_options(opts))
-end
-
-for _, mode in ipairs({ 'n', 'o', 'i', 'x', 't' }) do
-  M[mode .. 'map'] = function(...)
-    M.map(mode, ...)
-  end
-end
-
 M.buf_map = function(bufnr, mode, target, source, opts)
   opts = opts or {}
   opts.buffer = bufnr
 
-  M.map(mode, target, source, get_map_options(opts))
-end
-
-M.for_each = function(tbl, cb)
-  for _, v in ipairs(tbl) do
-    cb(v)
-  end
+  vim.keymap.set(mode, target, source, get_map_options(opts))
 end
 
 _G.inspect = function(...)
   print(vim.inspect(...))
 end
-
-M.timer = {
-  start_time = nil,
-  start = function()
-    M.timer.start_time = uv.now()
-  end,
-  stop = function()
-    print(uv.now() - M.timer.start_time .. ' ms')
-    M.timer.start_time = nil
-  end,
-
-  start_nano = function()
-    M.timer.start_time = uv.hrtime()
-  end,
-  stop_nano = function()
-    print(uv.hrtime() - M.timer.start_time .. ' ns')
-    M.timer.start_time = nil
-  end,
-}
 
 M.command = function(name, fn, opts)
   api.nvim_create_user_command(name, fn, opts or {})
@@ -66,40 +27,6 @@ end
 
 M.buf_command = function(bufnr, name, fn, opts)
   api.nvim_buf_create_user_command(bufnr, name, fn, opts or {})
-end
-
-M.t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-M.make_floating_window = function(custom_window_config, height_ratio, width_ratio)
-  height_ratio = height_ratio or 0.8
-  width_ratio = width_ratio or 0.8
-
-  local height = math.ceil(vim.opt.lines:get() * height_ratio)
-  local width = math.ceil(vim.opt.columns:get() * width_ratio)
-  local window_config = {
-    relative = 'editor',
-    style = 'minimal',
-    border = 'rounded',
-    width = width,
-    height = height,
-    row = width / 2,
-    col = height / 2,
-  }
-  window_config = vim.tbl_extend('force', window_config, custom_window_config or {})
-
-  local bufnr = api.nvim_create_buf(false, true)
-  local winnr = api.nvim_open_win(bufnr, true, window_config)
-  return winnr, bufnr
-end
-
-M.get_system_output = function(cmd)
-  return vim.split(vim.fn.system(cmd), '\n')
-end
-
-M.get_cwd = function()
-  return uv.cwd
 end
 
 M.table = {
