8566f168fe3d232eb618bfc412dec1c6c5bcf436
David Eriksson <david.eriksson@valtech.com>
2022-10-24 08:34:20 +0200

    fix: use gruvbuddy

---
+++ lua/modules/core/colors/gruvboxbby.lua
@@ -0,0 +1,13 @@
+-- Example config in Lua
+vim.g.gruvbox_baby_function_style = "italic"
+vim.g.gruvbox_baby_function_style = "bold"
+vim.g.gruvbox_baby_keyword_style = "italic"
+vim.g.gruvbox_baby_keyword_style = "bold"
+vim.g.gruvbox_baby_comment_style = "italic"
+
+-- Enable telescope theme
+vim.g.gruvbox_baby_telescope_theme = 1
+-- vim.g.gruvbox_baby_transparent_mode = 1
+
+-- Load the colorscheme
+vim.cmd[[colorscheme gruvbox-baby]]
