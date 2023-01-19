fccb19fb404d221b18e5b617ac8ccbecc09db70d
David Eriksson <david.eriksson@valtech.com>
2023-01-19 09:11:48 +0100

    fix(sumneko_lua): stop annoying workspace popup in lua files

--- lua/modules/lsp/sumneko_lua.lua
+++ lua/modules/lsp/sumneko_lua.lua
@@ -21,6 +21,7 @@ M.setup = function(on_attach, capabilities)
         },
         workspace = {
           -- Make the server aware of Neovim runtime files
+          checkThirdParty = false,
           library = vim.api.nvim_get_runtime_file('', true),
         },
         -- Do not send telemetry data containing a randomized but unique identifier
