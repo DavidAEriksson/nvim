0e75456347ed0cde4709d94cc1f0992297ae1006
David Eriksson <david.eriksson@valtech.com>
2023-01-19 09:12:19 +0100

    feat(treesitter): update incremental selection bindings

--- lua/modules/plugins/treesitter.lua
+++ lua/modules/plugins/treesitter.lua
@@ -22,10 +22,10 @@ require('nvim-treesitter.configs').setup({
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<leader><space>',
-      scope_incremental = 'grc',
-      node_decremental = '<c-backspace>',
+      init_selection = '<CR>',
+      node_incremental = '<CR>',
+      scope_incremental = '<S-CR>',
+      node_decremental = '<BS>',
     },
   },
   playground = {
