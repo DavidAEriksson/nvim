6e9f2306312f34e9a16a4d455e4c68d5a17193c5
David Eriksson <david.eriksson@valtech.com>
2023-04-20 08:36:55 +0200

    fix(neorg): update core modules to follow v3 breaking

--- after/plugin/neorg.lua
+++ after/plugin/neorg.lua
@@ -1,15 +1,15 @@
 require('neorg').setup({
   load = {
     ['core.defaults'] = {}, -- Loads default behaviour
-    ['core.norg.concealer'] = {}, -- Adds pretty icons to your documents
-    ['core.norg.dirman'] = { -- Manages Neorg workspaces
+    ['core.concealer'] = {}, -- Adds pretty icons to your documents
+    ['core.dirman'] = { -- Manages Neorg workspaces
       config = {
         workspaces = {
           notes = '~/Development/notes',
         },
       },
     },
-    ['core.norg.completion'] = {
+    ['core.completion'] = {
       config = {
         engine = 'nvim-cmp',
       },
