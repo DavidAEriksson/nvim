71d590be815e8b03e382a614acecd678d04f14b2
David Eriksson <david.eriksson@valtech.com>
2023-05-31 08:57:28 +0200

    fix(cursor_hold): update autocmd

--- lua/modules/core/autocmd/cursor_hold.lua
+++ lua/modules/core/autocmd/cursor_hold.lua
@@ -4,20 +4,13 @@ vim.api.nvim_create_autocmd('CursorHold', {
     local float_opts = {
       focusable = true,
       close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+      border = 'single',
+      source = 'always',
+      prefix = ' ',
+      scope = 'line',
+      pad_top = 1,
+      pad_bottom = 1,
     }
-
-    if not vim.b.diagnostics_pos then
-      vim.b.diagnostics_pos = { nil, nil }
-    end
-
-    local cursor_pos = vim.api.nvim_win_get_cursor(0)
-    if
-      (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
-      and #vim.diagnostic.get() > 0
-    then
-      vim.diagnostic.open_float(nil, float_opts)
-    end
-
-    vim.b.diagnostics_pos = cursor_pos
+    vim.diagnostic.open_float(nil, float_opts)
   end,
 })
