efb9db1708f33988455e090fb09cbeca3b05eff8
David Eriksson <david.eriksson@valtech.com>
2023-06-08 11:46:47 +0200

    feat(toggleterm): allow editor to override vim mappings

--- lua/modules/plugins/toggleterm.lua
+++ lua/modules/plugins/toggleterm.lua
@@ -1,3 +1,10 @@
 require('toggleterm').setup({
   start_in_insert = false,
 })
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
