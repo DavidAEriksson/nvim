f7f23997db3530c3a0886a54c9f894d3cee04525
David Eriksson <david.eriksson@valtech.com>
2023-07-04 09:00:59 +0200

    feat(colors): flatten github_light telescope theme

--- lua/modules/core/colors/github.lua
+++ lua/modules/core/colors/github.lua
@@ -21,6 +21,19 @@ github.setup({
   },
 })
 
--- FIX: This ugly ass telescope theme
-
 vim.cmd('colorscheme github_light')
+
+vim.api.nvim_set_hl(0, 'TelescopePromptPrefix', { bg = '#d0d7de' })
+vim.api.nvim_set_hl(0, 'TelescopePromptNormal', { bg = '#d0d7de' })
+vim.api.nvim_set_hl(0, 'TelescopePromptTitle', { bg = '#218bff', fg = '#d0d7de' })
+vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { bg = '#d0d7de', fg = '#d0d7de' })
+
+vim.api.nvim_set_hl(0, 'TelescopeResultsNormal', { bg = '#eaeef2' })
+vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', { bg = '#c297ff', fg = '#eaeef2' })
+vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { bg = '#eaeef2', fg = '#eaeef2' })
+
+vim.api.nvim_set_hl(0, 'TelescopePreviewNormal', { bg = '#f6f8fa' })
+vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', { bg = '#cf222e', fg = '#f6f8fa' })
+vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { bg = '#f6f8fa', fg = '#f6f8fa' })
+
+vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#218bff', fg = '#afb8c1' })
