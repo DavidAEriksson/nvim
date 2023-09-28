8b1106edb1454e331f18b48a18240bc5e3e5ffad
David Eriksson <davideriksson@daer3303chr.local>
2023-09-28 11:20:42 +0200

    fix(blankline): update setup to v3

--- after/plugin/blankline.lua
+++ after/plugin/blankline.lua
@@ -1,27 +1,7 @@
-local g = vim.g
+local ok, ibl = pcall(require, 'ibl')
 
-g.indent_blankline_char = '▏'
+if not ok then
+  return
+end
 
-g.indent_blankline_use_treesitter = true
-g.indent_blankline_show_current_context = true
-
-g.indent_blankline_show_first_indent_level = true
-g.indent_blankline_show_trailing_blankline_indent = true
-
-g.indent_blankline_context_patterns = {
-  'class',
-  'function',
-  'method',
-  '^if',
-  '^while',
-  '^for',
-  '^object',
-  '^table',
-  'block',
-  'arguments',
-}
-
-g.indent_blankline_filetype_exclude = { 'help', 'packer', 'terminal' }
-g.indent_blankline_buftype_exclude = { 'help', 'terminal', 'nofile' }
-g.indent_blankline_char_highlight = 'LineNr'
-g.indent_blankline_max_indent_increase = 1
+ibl.setup({})
