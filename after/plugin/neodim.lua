cbbf5ea58f3ed6c3d8e98bbdf8e843b0db689e18
David Eriksson <davideriksson@daer3303chr.local>
2023-10-06 09:05:53 +0200

    chore: cleanup

--- after/plugin/neodim.lua
+++ after/plugin/neodim.lua
@@ -4,12 +4,12 @@ if not ok then
   return
 end
 
---[[ neodim.setup({ ]]
---[[   alpha = 0.45, ]]
---[[   blend_color = '#1e222a', ]]
---[[   hide = { ]]
---[[     virtual_text = true, ]]
---[[     signs = true, ]]
---[[     underline = true, ]]
---[[   }, ]]
---[[ }) ]]
+neodim.setup({
+  alpha = 0.45,
+  blend_color = '#1e222a',
+  hide = {
+    virtual_text = true,
+    signs = true,
+    underline = true,
+  },
+})
