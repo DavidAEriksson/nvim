6be2c535e68ce327f0ccc721c9d0198761ff0b23
David Eriksson <davideriksson@daer3303chr.local>
2023-10-10 11:37:22 +0200

    feat(rell): set shiftwidth to 4 for rell files

--- lua/modules/core/autocmd/rell_ft.lua
+++ lua/modules/core/autocmd/rell_ft.lua
@@ -2,3 +2,8 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'BufRead' }, {
   pattern = { '*.rell' },
   command = 'set filetype=rell',
 })
+
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter', 'BufRead' }, {
+  pattern = { '*.rell' },
+  command = 'set shiftwidth=4',
+})
