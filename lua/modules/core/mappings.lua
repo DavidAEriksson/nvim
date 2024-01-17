f2a4a63f42e72aa263fcdcfece293887d3b0f5b2
David Eriksson <davideriksson@daer3303chr.lan>
2024-01-17 09:14:54 +0100

    feat(mappings): add scissors edit mapping

--- lua/modules/core/mappings.lua
+++ lua/modules/core/mappings.lua
@@ -171,3 +171,7 @@ end, {
 vim.keymap.set({ 'n', 'x' }, '<leader>sa', function()
   require('scissors').addNewSnippet()
 end)
+
+vim.keymap.set('n', '<leader>se', function()
+  require('scissors').editSnippet()
+end)
