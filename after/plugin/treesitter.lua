78e7cc3cc2fecafa8296731674a825d4cf1aa849
David Eriksson <david.eriksson@chromaway.com>
2024-03-21 16:23:24 +0100

    feat(treesitter): ignore highlighting in very large files

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -19,6 +19,14 @@ require('nvim-treesitter.configs').setup({
   auto_install = true,
   highlight = {
     enable = true,
+    disable = function(_, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local filename = vim.api.nvim_buf_get_name(buf)
+      local ok, stats = pcall(vim.uv.fs_stat, filename)
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
     additional_vim_regex_highlighting = false,
   },
   autotag = {
