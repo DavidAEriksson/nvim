579cfae87cc7494d0556e19b6300d0d9e8f6ff3d
David Eriksson <david.eriksson@chromaway.com>
2024-06-27 14:32:18 +0200

    fix(gitsigns): remove deprecated highlights

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -5,13 +5,6 @@ if not ok then
 end
 
 gitsigns.setup({
-  signs = {
-    add = { hl = 'GitSignsAdd', text = '+', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-    change = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-    delete = { hl = 'GitSignsDelete', text = '-', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    topdelete = { hl = 'GitSignsDelete', text = '-', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-    changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-  },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
@@ -40,9 +33,6 @@ gitsigns.setup({
     relative = 'cursor',
     row = 0,
     col = 1,
-  },
-  yadm = {
-    enable = false,
   },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
