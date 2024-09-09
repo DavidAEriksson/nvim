ce6213c4b99fa662aa67ebd59dbc2f5e89876319
David Eriksson <david.eriksson@chromaway.com>
2024-09-09 13:30:49 +0200

    chore(lsp): fix tsserver config since its now ts_ls

--- lua/modules/lsp/tsserver.lua
+++ lua/modules/lsp/tsserver.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup = function(on_attach, capabilities)
   local lspconfig = require('lspconfig')
 
-  lspconfig['tsserver'].setup({
+  lspconfig['ts_ls'].setup({
     root_dir = lspconfig.util.root_pattern('package.json'),
     on_attach = on_attach,
     capabilities = capabilities,
